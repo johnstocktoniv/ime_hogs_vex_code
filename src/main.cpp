bfb9dc39df7dc58fe327141a0679ff8966cf359f
John W Stockton <johnstocktoniv@JWS-MacBook-Pro.local>
2021-01-21 16:14:29 -0500

    project

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       johnstocktoniv                                            */
+/*    Created:      Thu Jan 21 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
