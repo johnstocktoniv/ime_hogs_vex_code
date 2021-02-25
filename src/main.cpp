e0f93e6f36063baa28b3a07bf1213a35552b696e
John Stockton <JohnStocktonIV@gmail.com>
2021-02-25 14:02:49 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -19,29 +19,31 @@ using namespace vex;
 
 
 void Auto() {
+  int driveVelocity = 50;
+  int turnVelocity = 50;
 
   //Getting the soccer balls
-  Drivetrain.driveFor(-55, inches, 5, rpm);
-  Drivetrain.turnFor(left, 90, degrees, 5, rpm);
-  Drivetrain.driveFor(-15, inches, 5, rpm);
-  Drivetrain.driveFor(25, inches, 5, rpm);
-  Drivetrain.turnFor(left, 90, degrees, 5, rpm);
-  Drivetrain.driveFor(5, inches, 5, rpm);
+  Drivetrain.driveFor(-55, inches, driveVelocity, rpm);
+  Drivetrain.turnFor(left, 90, degrees, turnVelocity, rpm);
+  Drivetrain.driveFor(-15, inches, driveVelocity, rpm);
+  Drivetrain.driveFor(25, inches, driveVelocity, rpm);
+  Drivetrain.turnFor(left, 90, degrees, turnVelocity, rpm);
+  Drivetrain.driveFor(5, inches, driveVelocity, rpm);
 
   //Getting the footballs
   for(int i = 0; i < 3; i++) {
     //Grabbing & Dropping Ball
     Claw.rotateFor(1, sec, 1, rpm);
     Arm.startRotateFor(fwd, 360, degrees, 1, rpm);
-    Drivetrain.driveFor(-45, inches, 5, rpm);
+    Drivetrain.driveFor(-45, inches, driveVelocity, rpm);
     Drivetrain.turnFor(left, 90, degrees, 5, rpm);
-    Drivetrain.driveFor(25, inches, 5, rpm);
+    Drivetrain.driveFor(25, inches, driveVelocity, rpm);
     Claw.rotateFor(-1, sec, 1, rpm);
     //Returning to start
-    Drivetrain.driveFor(25, inches, 5, rpm);
+    Drivetrain.driveFor(25, inches, driveVelocity, rpm);
     Drivetrain.turnFor(right, 90, degrees, 5, rpm);
     Arm.startRotateFor(reverse, 360, degrees, 1, rpm);
-    Drivetrain.driveFor(45, inches, 5, rpm);
+    Drivetrain.driveFor(45, inches, driveVelocity, rpm);
   }
 
 }
@@ -52,11 +54,14 @@ int main() {
  // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  Drivetrain.setDriveVelocity(50, percent);
-  Drivetrain.setTurnVelocity(50, percent);
+  Drivetrain.setDriveVelocity(1, percent);
+  Drivetrain.setTurnVelocity(1, percent);
 
   while(true) {
-    if(Controller1.ButtonUp.pressing()) Auto();
+    if(Controller1.ButtonUp.pressing()) {
+      Auto();
+      break;
+    }
   }
 
 }
