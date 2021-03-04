56ebd1737c4c923a13ab3dc4df5ff04bb3ea4983
John Stockton <JohnStocktonIV@gmail.com>
2021-03-04 15:34:49 -0500

    Updated the algorithm for autonomous, you should only have to modify the numbers Chance

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ void turnToHeading( double angle, turnType direction) {
   while(true) {
     Brain.Screen.print(Gyro1.heading());
     Brain.Screen.newLine();
-    if(Gyro1.heading() >= angle-2 && Gyro1.heading() <= angle+2) {
+    if(Gyro1.heading() >= angle-1 && Gyro1.heading() <= angle+1) {
       Drivetrain.stop();
       break;
     }
@@ -31,33 +31,31 @@ void turnToHeading( double angle, turnType direction) {
 
 void Auto() {
   int driveVelocity = 25;
-  int turnVelocity = 70;
+  int distBetweenFootballs = 1; //This will need to be fiddled with, but it corresponds to the distance b/t the footballs on the wooden thingy
 
   //Getting the soccer balls
-  Drivetrain.driveFor(-110, inches, driveVelocity, rpm);
-  turnToHeading(90, right);
+  Drivetrain.driveFor(110, inches, driveVelocity, rpm);
+  turnToHeading(-90, left);
+  Drivetrain.driveFor(15, inches, driveVelocity, rpm);
+  //Reset to start getting the footballs
   Drivetrain.driveFor(-15, inches, driveVelocity, rpm);
-  Drivetrain.driveFor(25, inches, driveVelocity, rpm);
-  Drivetrain.turnFor(left, 500, degrees, turnVelocity, rpm);
-  turnToHeading(90, right);
+  turnToHeading(0, right);
   Drivetrain.driveFor(5, inches, driveVelocity, rpm);
-  Gyro1.setHeading(0, degrees);
   //Getting the footballs
   for(int i = 0; i < 3; i++) {
     //Grabbing & Dropping Ball
-    Claw.rotateFor(1, sec, 1, rpm);
-    Arm.startRotateFor(fwd, 360, degrees, 1, rpm);
-    Drivetrain.driveFor(-45, inches, driveVelocity, rpm);
-    Drivetrain.turnFor(left, 500, degrees, 5, rpm);
-    Drivetrain.driveFor(25, inches, driveVelocity, rpm);
-    Claw.rotateFor(-1, sec, 1, rpm);
+    Claw.rotateFor(1, sec, 1, rpm); //Grab Ball
+    Arm.startRotateFor(fwd, 360, degrees, 1, rpm); //Move arm up into position
+    Drivetrain.driveFor(-45, inches, driveVelocity, rpm); //back up to the basket
+    turnToHeading(-90, left); //Turn toward the basket
+    Drivetrain.driveFor(25+(i*distBetweenFootballs), inches, driveVelocity, rpm); //Drive up to the basket
+    Claw.rotateFor(-1, sec, 1, rpm); //Release ball
     //Returning to start
-    Drivetrain.driveFor(25, inches, driveVelocity, rpm);
-    Drivetrain.turnFor(right, 500, degrees, 5, rpm);
+    Drivetrain.driveFor(-25-(i*distBetweenFootballs), inches, driveVelocity, rpm);
+    turnToHeading(0, right);
     Arm.startRotateFor(reverse, 360, degrees, 1, rpm);
     Drivetrain.driveFor(45, inches, driveVelocity, rpm);
   }
-  */
 }
 
 
@@ -73,5 +71,9 @@ int main() {
     }
   }
 
+  while(true) {
+    Brain.Screen.print("EAT BUTTS DWEEBS");
+    Brain.Screen.newLine();
+  }
 
 }
